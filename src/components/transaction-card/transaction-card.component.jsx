3b399c2537df5ed89ffd8dea55728568a3fa5b1c
tom-blk <bleek.tom@gmail.com>
2023-01-02 22:31:32 +0100

    started with pending transaction state management

--- src/components/transaction-card/transaction-card.component.jsx
+++ src/components/transaction-card/transaction-card.component.jsx
@@ -1,7 +1,16 @@
 import { services } from "../../datamodels/services/services-examples";
 import { members } from "../../datamodels/members/members-examples";
+import { useEffect, useState } from "react";
 
 const TransactionCard = ({transaction}) => {
+
+    useEffect(() => {
+        if(transaction.completed === false){
+            setTransactionIsComplete(false);
+        }
+    }, [])
+
+    const [transactionIsComplete, setTransactionIsComplete] = useState(false);
 
     const findService = () => {
         return(services.find(service => service.id === transaction.serviceId).name)
@@ -12,6 +21,22 @@ const TransactionCard = ({transaction}) => {
         return(providingUser.firstName + " " + providingUser.lastName)
     }
 
+    const modifyTransactionStatus = () => {
+        transaction.completed = !transaction.completed
+    }
+
+    const renderTransactionText = () => {
+        if(transactionIsComplete){
+            return(
+                "Done!"
+            )
+        } else {
+            return(
+                "Complete Transaction"
+            )
+        }
+    }
+
     return(
         <div className="card">
             <div>{`Date Issued: ${transaction.dateIssued}`}</div>
@@ -19,7 +44,12 @@ const TransactionCard = ({transaction}) => {
             <div>{transaction.receivingUserId}</div>
             <div>{`Provided by: ${findProvidingUser()}`}</div>
             <div>{`Credits Awarded: ${transaction.creditsAwarded}`}</div>
-            <div className="button confirm-button">Click Here If Transaction is Complete</div>
+            <div 
+                className="button confirm-button"
+                onClick={e => modifyTransactionStatus()}
+            >
+                {renderTransactionText()}
+            </div>
         </div>
     )
 }
