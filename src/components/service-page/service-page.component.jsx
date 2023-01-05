5699f67a71bc7fed2b5ec5908e36995f5e42be50
tom-blk <bleek.tom@gmail.com>
2023-01-05 16:41:34 +0100

    improved single service overview page

--- src/components/service-page/service-page.component.jsx
+++ src/components/service-page/service-page.component.jsx
@@ -1,6 +1,10 @@
 import { Fragment } from "react";
 import { useParams } from "react-router"
+import { members } from "../../datamodels/members/members-examples";
 import { services } from "../../datamodels/services/services-examples";
+import PageContainer from "../../utils/page-container/page-container.component";
+import RoundImageContainer from "../profile-avatar/round-image-container.component";
+import ProviderCard from "../provider-card/provider-card.component";
 
 const ServicePage = () => {
 
@@ -10,15 +14,29 @@ const ServicePage = () => {
     
     const currentService = services.find(service => service.id === serviceIdInt)
 
+    const serviceProviders = members.filter(member => {return(member.providableServices.includes(currentService.id))})
+
     return(
         <Fragment>
             {
                 currentService != undefined
                 ?
-                <div>
-                    <div className="service-icon" style={{backgroundColor: "blue", height: "50px", width: "50px"}}></div>
-                    <div>{currentService.id + " " + currentService.name}</div>
-                </div>
+                <PageContainer>
+                    <div className="transaction-page-heading">
+                        <RoundImageContainer picture={currentService.icon} size={'page'}/>
+                        <div className="heading-primary">{currentService.name}</div>
+                    </div>
+                    <div className="heading-secondary">{`Credits per Hour: ${currentService.creditsPerHour}`}</div>
+                    <div className="text">{currentService.description}</div>
+                    <div className="heading-secondary">Providers:</div>
+                    {
+                        serviceProviders.map(provider => {
+                            return(
+                                <ProviderCard provider={provider}/>
+                            )
+                        })
+                    }
+                </PageContainer>
                 :
                 <div>Sorry, but there is nothing here...</div>
             }
--- src/components/transaction-page/transaction-page.component.jsx
+++ src/components/transaction-page/transaction-page.component.jsx
@@ -5,6 +5,8 @@ import { members } from "../../datamodels/members/members-examples";
 import { services } from "../../datamodels/services/services-examples";
 import RoundImageContainer from "../profile-avatar/round-image-container.component";
 
+import "./transaction-page.styles.scss"
+
 const TransactionPage = () => {
 
     const { transactionId } = useParams();
@@ -21,11 +23,14 @@ const TransactionPage = () => {
 
     return(
         <PageContainer>
-            <RoundImageContainer picture={providedService.icon} size={'page'}/>
-            <div className="heading-primary">{`Order Of ${providedService.name} at ${currentTransaction.dateIssued}`}</div>
+            <div className="transaction-page-heading">
+                <RoundImageContainer picture={providedService.icon} size={'page'}/>
+                <div className="heading-primary">{`Order Of ${providedService.name} at ${currentTransaction.dateIssued}`}</div>
+            </div>
             <div className="text">{`service recipient: ${transactionRecipient.firstName} ${transactionRecipient.lastName}`}</div>
             <div className="text">{`service provider: ${transactionProvider.firstName} ${transactionProvider.lastName}`}</div>
             <div className="text">{`credits awarded: ${currentTransaction.creditsAwarded}`}</div>
+            <div className="button confirm-button">Complete Transaction</div>
         </PageContainer>
     )
 }
