2d0b50394ee0a582f95619aec1962ee87ef80050
Tom Bleek <tom-blk>
2023-06-09 16:40:43 +0200

    fixed issue in new design

--- src/utils/background/background.component.jsx
+++ src/utils/background/background.component.jsx
@@ -4,7 +4,9 @@ import './background.styles.scss';
 const Background = ({children}) => {
   return (
     <div className='wrapper'>
-        <div className='background-component'/>
+        <div className='black-background-wrapper'>
+            <div className='background-component'/>
+        </div>
         <div className='foreground'>
             {children}
         </div>
