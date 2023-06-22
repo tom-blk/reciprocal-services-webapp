a032308e528e3facf095ad2dffc1db3d989605f4
Tom Bleek <tom-blk>
2023-06-22 16:06:16 +0200

    Refactored button components and styles, code is way more concise now

---
+++ src/components/card-lists/card-list/card-list.component.jsx
@@ -0,0 +1,22 @@
+const CardList = ({ list, OutputCard, expectedPropByOutputCardString, emptyListWarning}) => {
+
+    return(
+        <div className="card-list">
+            {
+                Array.isArray(list) &&
+                list.length > 0
+                ?
+                list.map((listItem) => {
+                    return(
+                            <OutputCard key={listItem.id} item={listItem}/>
+                        ) 
+                    } 
+                )
+                :
+                <div className="text">{emptyListWarning}</div>
+            }
+        </div>
+    )
+}
+
+export default CardList
