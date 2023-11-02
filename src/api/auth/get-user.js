34d9ecb1c455c0f3fc534084b0ea90425053b999
tom-blk <tom.bleek@proton.me>
2023-11-02 16:34:21 +0100

    Applied backend server name

--- src/api/auth/get-user.js
+++ src/api/auth/get-user.js
@@ -2,7 +2,7 @@ import { apiCall } from "../api-call";
 
 export const getUser = async () => {
         try{
-            const data = await apiCall('/auth/get-user', 'POST');
+            const data = await apiCall('/auth/get-user', 'GET');
             return data;
         } catch(error){
             throw new Error(error);
