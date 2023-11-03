bd9ffebfe375a91d755b1874c2b3fc64c7dc0a99
tom-blk <tom.bleek@proton.me>
2023-11-03 13:40:07 +0100

    Applied missing domain name to api calls

--- src/api/auth/log-out.js
+++ src/api/auth/log-out.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const logOut = async (email, password) => {
     
     try{
-        const response = await axios.post(`http://141.147.52.47/auth/log-out`, {}, {withCredentials: true})
+        const response = await axios.post(`http://prometheus-backend.top/auth/log-out`, {}, {withCredentials: true})
         return response;
     } catch(error){
         throw new Error(error.response.data)
