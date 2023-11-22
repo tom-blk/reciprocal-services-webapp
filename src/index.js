0c3660f5cd9507b54159bb4e435ae8fe1da8c8f4
Tom Bleek <tom-blk>
2023-11-22 15:40:00 +0100

    different approach on hash router 3

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import Background from './utils/background/background.component';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/prometheus'>
+    <HashRouter basename='/'>
       <AlertMessageContextProvider>
         <UserContextProvider>
           <OrderContextProvider>
